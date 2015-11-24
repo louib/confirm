2a080d41a2ade66ae7f0eced7bd596552a72a71d
Louis-Bertrand Varin <louisbvarin@gmail.com>
2015-11-24 15:37:28 -0500

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from setuptools import setup
+import os
+import os.path
+
+version = 'dev'
+
+if os.path.exists('version'):
+  version = file('version').readline()[:-1]
+
+setup(
+      name="confirm",
+      version=version,
+      description="Simple Python configuration file validation.",
+      author="Louis-Bertrand Varin",
+      author_email="louisbvarin@gmail.com",
+      packages=['confirm',]
+     )
