1281f7d33a8c5dec593e926b1d4f206677a8f5a9
Ronan Mejecaze <echoz38@gmail.com>
2015-11-25 14:58:40 -0500

    Rework setup + reqs

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
-from setuptools import setup
 import os
-import os.path
+from setuptools import setup
 
-version = 'dev'
-
-if os.path.exists('version'):
-  version = file('version').readline()[:-1]
+LONG_DESC = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
 
 setup(
       name="confirm",
-      version=version,
+      version="0.1.0",
       description="Simple Python configuration file management.",
+      long_description=LONG_DESC,
       author="Louis-Bertrand Varin",
       author_email="louisbvarin@gmail.com",
-      packages=['confirm',]
+      packages=['confirm',],
+      install_requires=['PyYAML>=3.0',],
+      classifiers=[
+          'Programming Language :: Python',
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+          'Topic :: Utilities',
+      ],
      )
