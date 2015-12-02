ffbc60dbf9dc66dd44dd338c324cdb7a2c5efce5
Louis-Bertrand Varin <louisbvarin@gmail.com>
2015-12-02 10:46:24 -0500

    Adding flake8.

--- scripts/generate.py
+++ scripts/generate.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import sys
 import argparse
-from ConfigParser import SafeConfigParser
 
 import yaml
 
--- scripts/validate.py
+++ scripts/validate.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import sys
 import argparse
 from ConfigParser import SafeConfigParser
 
